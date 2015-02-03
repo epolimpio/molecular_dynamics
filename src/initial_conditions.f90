b22c4aa8b4e6d5d23f23845f7bc5257e6665aeef
Eduardo Olimpio <epolimpio@gmail.com>
2015-02-03 00:44:13 +0000

    Introducing gaussian random generator

--- src/initial_conditions.f90
+++ src/initial_conditions.f90
@@ -7,26 +7,61 @@ module initial_conditions
 
 contains
 
-	function initial_mom(N, energy) result(mom)
+	function initial_mom(N, temp) result(mom)
 
-		real(8), intent(in) :: energy, N
+		real(8), intent(in) :: temp
+		integer(8), intent(in) :: N
 		real(8) :: rnd
-
-		call init_random_seed
-
-		! Get a random_number in order to pickup
-		! a value in the Gaussian distribution
-		rnd = random_number
 
 
 
 	end function initial_mom
 
-	function max_boltz(rnd) result(velocity)
 
-		
+	function max_boltz(N, temp) result(velocity)
+
+		real(8), intent(in) :: temp
+		real(8), parameter :: PI = 4*atan(1d0)
+		real(8) :: velocity(3, N)
+		integer :: i, j
+		real(8) :: v_sum, v_rnd, var_v
+
+		! We use Gaussian random to generate the particle velocity 
+		! in each direction for N-1 particles. The last particle 
+		! velocity is such that the sum is zero
+		do i = 1, 3
+			v_sum = 0
+			do j = 1, N-1
+				v_rnd = box_muller
+				velocity(i, j) = v_rnd
+				v_sum = v_sum + v_rnd
+			end do
+		end do
 
 	end function max_boltz
+
+	! Implementation of polar Box-Muller algorithm
+	! to generate Gaussian Random numbers
+
+	real(8) function box_muller() result(gauss_number)
+
+		real(8) :: x1, x2, w, y1, y2
+
+		call init_random_seed
+
+		do
+			x1 = 2d0*random_number - 1
+			x2 = 2d0*random_number - 1
+			w = x1 * x1 + x2 * x2
+		until w <= 1d0
+
+		gauss_number = sqrt( (-2d0 * log(w)) / w )
+
+	end function box_muller
+
+
+	! Function to initialize the seed of the random
+	! number generator
 
 	subroutine init_random_seed()
 
